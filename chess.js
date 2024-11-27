e115f912e28b7664937449df4180b2010ca31d19
BudayBoldi <78080884+BudayBoldi@users.noreply.github.com>
2024-11-27 19:57:22 +0100

    Update chess.js

--- chess.js
+++ chess.js
@@ -21,6 +21,10 @@ switch (x) {
         break;
 }
 
+function ShowMoves(from) {
+    console.log(from);
+}
+
 const wchars = ["A",  "C",  "E",  "G"];
 const bchars = ["B", "D", "F", "H"];
 let counter = 1;
@@ -28,27 +32,27 @@ let counter = 1;
 do {
     if (counter %2 != 0) {
         wchars.forEach(function(item) {
-            console.log(item);
-            let wchar = document.querySelector("." + item + counter);
+            let wchar = document.getElementById(item + counter);
+            wchar.onclick = function () { ShowMoves(this.id) };
             wchar.style.cssText = "float: left;width: 40px;height: 40px;background-color: #fff;font-size: 25px;text-align: center;display: table-cell;vertical-align: middle";
             wchar.innerHTML = wx;
         });
         bchars.forEach(function(item) {
-            console.log(item);
-            let bchar = document.querySelector("." + item + counter);
+            let bchar = document.getElementById(item + counter);
+            bchar.onclick = function () { ShowMoves(this.id) };
             bchar.style.cssText = "float: left;width: 40px;height: 40px;background-color: #999;font-size: 25px;text-align: center;display: table-cell;vertical-align: middle";
             bchar.innerHTML = bx;
         });
     } else {
         wchars.forEach(function(item) {
-            console.log(item);
-            let wchar = document.querySelector("." + item + counter);
+            let wchar = document.getElementById(item + counter);
+            wchar.onclick = function () { ShowMoves(this.id) };
             wchar.style.cssText = "float: left;width: 40px;height: 40px;background-color: #999;font-size: 25px;text-align: center;display: table-cell;vertical-align: middle";
             wchar.innerHTML = bx;
         });
         bchars.forEach(function(item) {
-            console.log(item);
-            let bchar = document.querySelector("." + item + counter);
+            let bchar = document.getElementById(item + counter);
+            bchar.onclick = function () { ShowMoves(this.id) };
             bchar.style.cssText = "float: left;width: 40px;height: 40px;background-color: #fff;font-size: 25px;text-align: center;display: table-cell;vertical-align: middle";
             bchar.innerHTML = wx;
         });
